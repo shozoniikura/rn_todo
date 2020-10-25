1cd9e1e1cf329e9c8163bd202927bdc4918b3a29
NIIKURA Shozo <niikura@nyam.co.jp>
2020-10-26 01:39:27 +0900

    feat: データの削除を実行できるようにしました。

--- components/ToDo.js
+++ components/ToDo.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, CheckBox } from 'react-native';
+
+// import WebAPI
+import {ToDoRecord} from '../modules/webAPI';
 
 export default function ToDo(props) {
   const {todo} = props;
   // console.log(props);
   return (
     <View style={styles.todo}>
+      <CheckBox onValueChange={(x)=> {console.log(todo);todo.done()}} />
       <Text style={styles.todoText}>{todo.title}</Text>
     </View>
   );
--- modules/constants.js
+++ modules/constants.js
@@ -2,8 +2,10 @@ const constants = {
   proto: 'http://',
   host: '10.0.3.2:14330',
   pathToDos: '/to_dos.json',
+  pathToDoBase: '/to_dos/',
 };
 
 constants.urlToDos = constants.proto + constants.host + constants.pathToDos;
+constants.urlToDoBase = constants.proto + constants.host + constants.pathToDoBase;
 
 export default constants;
