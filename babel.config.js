df1e4ff751047c9034ab8b90de7a6bb2106f0574
NIIKURA Shozo <niikura@nyam.co.jp>
2020-10-26 21:04:55 +0900

    dotenvを使えるようにしました。

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,16 @@
 module.exports = function(api) {
   api.cache(true);
   return {
+    "plugins": [
+      ["module:react-native-dotenv", {
+        "moduleName": "@env",
+        "path": ".env",
+        "blacklist": null,
+        "whitelist": null,
+        "safe": false,
+        "allowUndefined": true
+      }]
+    ],
     presets: ['babel-preset-expo'],
   };
 };
